e4d89eaafb4571a49b8e2dc8141a825c50340aa7
Simon Ingeson <simon.ingeson@ef.com>
2015-11-12 10:26:51 +0100

    added css preprocessing (less)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
 
-gulp.task('default', function () {
+gulp.task('styles', function () {
+  return gulp.src(['./src/styles/site.less'])
+    .pipe(less())
+    .pipe(gulp.dest('./wwwroot/styles/'));
+});
+
+gulp.task('html', function () {
   return gulp.src(['./src/index.html'])
     .pipe(gulp.dest('./wwwroot/'));
 });
+
+gulp.task('default', ['html', 'styles']);
